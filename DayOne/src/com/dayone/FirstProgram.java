a8d7a0be58725d6a25430473c4d528711cb8c148
suyash <suyashshinde76@gmail.com>
2025-01-28 14:17:00 +0530

    my first commit

---
+++ DayOne/src/com/dayone/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.dayone;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is my first progam");
+		System.out.println("my name is suyash");
+		System.out.println("my age is 21");
+	}
+
+}
