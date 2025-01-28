47b8ab9362130f76ba3f9bfc10e6bc19d789dd2f
suyash <suyashshinde76@gmail.com>
2025-01-28 14:42:21 +0530

    this is my second day

---
+++ DayTwo/src/com/daytwo/DayTwo.java
@@ -0,0 +1,11 @@
+package com.daytwo;
+
+public class DayTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is day two");
+		System.out.println("created github repo");
+	}
+
+}
