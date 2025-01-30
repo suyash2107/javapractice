97234705c1157cc430db7517b95f1c2190de6db3
suyash <suyashshinde76@gmail.com>
2025-01-30 13:58:35 +0530

    day three done with variable declartion and datatypes

---
+++ DayThree/src/DayThree.java
@@ -0,0 +1,54 @@
+
+public class DayThree {
+
+	public static void main(String[] args) {
+		System.out.println("this is day three");
+		System.out.println("varable declaration");
+		//varable declaration
+		int a=90;
+		int b=10;
+		
+		int sum =a + b;
+		System.out.println(sum);
+		
+		float value1 =1111f/20.2f;
+		System.out.println("float value"+ value1);
+		
+		double value2=10000d/50d;
+		System.out.println("double value"+ value2);
+		
+		char name ='s';
+		System.out.println(name);
+		
+		
+		//byte takes 1byte
+		byte byteMax=127;
+		byte byteMin=-128;
+		System.out.println("min range of byte"+ byteMin);
+		System.out.println("max range of byte"+ byteMax);
+		
+		//short 2 byte
+		short shortMax =32767;
+		short shortMin=-32767;
+		System.out.println("min range of short"+ shortMin);
+		System.out.println("min range of short"+ shortMax);
+		
+		// int 4 byte
+		
+		int intMax = 2147483647;
+		int intMin = -2147483648;
+		
+		
+		// long 8 byte
+		
+		long longMin = 9223372036854775808L;
+		long longMax = -9223372036854775807L;
+
+
+		
+
+
+	}
+
+}
+		
